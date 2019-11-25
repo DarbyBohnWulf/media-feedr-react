24593dab4cfa072fffa9aaba17a1a3f6fc644ba0
Mark Ritchie <mwritchie2@gmail.com>
2019-11-25 11:18:35 -0600

    styled search results to show posters if available

--- src/SearchResult.jsx
+++ src/SearchResult.jsx
@@ -4,8 +4,16 @@ import CardHeader from '@material-ui/core/CardHeader';
 import CardMedia from '@material-ui/core/CardMedia';
 import CardContent from '@material-ui/core/CardContent';
 import Typography from '@material-ui/core/Typography';
+import { makeStyles } from '@material-ui/core/styles';
+
+const useStyles = makeStyles(theme => ({
+  media: {
+    height: 278,
+  },
+}));
 
 function SearchResult(props) {
+  const classes = useStyles();
   const tmdbBaseUrl = 'https://image.tmdb.org/t/p/w185';
   const title = props.result.title ? props.result.title : props.result.name
   return (
@@ -15,8 +23,8 @@ function SearchResult(props) {
         subheader={props.result.release_date} />
       <CardMedia
         image={tmdbBaseUrl + props.result.poster_path}
-        mediaStyle={'height: 278px'}
-        title={'Poster for ' + title} />
+        title={'Poster for ' + title}
+        className={classes.media} />
       <CardContent>
         <Typography variant='body1'>
           {props.result.overview}
